4d2052a8d10ff51438faeac31dbb4eccabaeaaca
Van Luong <van.luong@opswat.com>
2025-04-22 11:36:22 +0700

    Add new unit tests for new cases in core database module

--- core/database/src/test/java/com/vanluong/database/GithubUserDatabaseTest.kt
+++ core/database/src/test/java/com/vanluong/database/GithubUserDatabaseTest.kt
@@ -5,8 +5,10 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import com.vanluong.database.entity.mapper.UserEntityMapper.toEntityList
 import com.vanluong.testing.MockDataUtil
+import junit.framework.TestCase.assertNotNull
 import kotlinx.coroutines.test.runTest
 import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -41,18 +43,33 @@ class GithubUserDatabaseTest {
         val userEntityList = MockDataUtil.mockGithubUserList().toEntityList()
         dao.insertUsers(userEntityList)
 
-//        val pagingSource = dao.getUsersPaged()
-//        val loadResult = pagingSource.load(
-//            PagingSource.LoadParams.Refresh(
-//                key = null,
-//                loadSize = userEntityList.size,
-//                placeholdersEnabled = false
-//            )
-//        )
-//
-//        assertTrue(loadResult is PagingSource.LoadResult.Page)
-//        val page = loadResult as PagingSource.LoadResult.Page
-//        assertEquals(userEntityList.size, page.data.size)
-//        assertEquals(userEntityList, page.data)
+        val fetchedUser = dao.getUserById(101)
+        assertNotNull(fetchedUser)
+        assertEquals(userEntityList[0].id, fetchedUser.id)
+        assertEquals(userEntityList[0].login, fetchedUser.login)
+    }
+
+    @Test
+    fun fetchPagedUsersTest() = runTest {
+        val userEntityList = MockDataUtil.mockGithubUserList().toEntityList()
+        dao.insertUsers(userEntityList)
+
+        val pagedUsers = dao.getUsersPaged(limit = 5, offset = 0)
+        assertEquals(2, pagedUsers.size)
+        assertEquals("jvantuyl", pagedUsers[0].login)
+        assertEquals("BrianTheCoder", pagedUsers[1].login)
+    }
+
+    @Test
+    fun updateUserTest() = runTest {
+        val userEntityList = MockDataUtil.mockGithubUserList().toEntityList()
+        dao.insertUsers(userEntityList)
+
+        val updatedUser = userEntityList[0].copy(location = "Mars", followers = 200)
+        dao.updateUser(updatedUser)
+
+        val fetchedUser = dao.getUserById(101)
+        assertEquals("Mars", fetchedUser.location)
+        assertEquals(200, fetchedUser.followers)
     }
 }
